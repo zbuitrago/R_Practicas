9a9916dd2ef66514b13323226944938871558eb0
LAPTOP-PAI581F4\bdih7 <jd.bermeo251@gmail.com>
2019-09-04 11:25:00 -0500

    Structure

---
+++ Scripts/Codigo.R
@@ -0,0 +1,10 @@
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+
+df <- read.csv("C:/Users/bdih7/Desktop/Measurement Matters/Data_Check _Trabajadores_Colombia.csv", encoding = "UTF-8",stringsAsFactors = FALSE)
+df
+hist(jump_scares$jump_score)
+install.packages("profvis")
+
+install.packages("proto")
