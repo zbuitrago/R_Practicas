427ac3891ab76cc51bb30961473e345db46a6519
LAPTOP-PAI581F4\bdih7 <jd.bermeo251@gmail.com>
2019-09-04 12:49:46 -0500

    promedio en dataframe

--- Scripts/Codigo.R
+++ Scripts/Codigo.R
@@ -1,10 +1,10 @@
 library(dplyr)
 library(ggplot2)
 library(tidyr)
-
-df <- read.csv("C:/Users/bdih7/Desktop/Measurement Matters/Data_Check _Trabajadores_Colombia.csv", encoding = "UTF-8",stringsAsFactors = FALSE)
-df
-hist(jump_scares$jump_score)
-install.packages("profvis")
-
-install.packages("proto")
+# carga de dataset
+df <- read.csv("C:/Users/bdih7/Desktop/Modificaciones/R/Data/input/jump_scare_movies.csv", encoding = "UTF-8",stringsAsFactors = FALSE)
+df 
+# Use a function
+# 1. mean () // sacar el promedio en un dataframe
+mean_jump <- mean(df[["jump_count"]])
+mean_jump
